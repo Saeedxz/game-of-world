6963730a8b4d4612cd82ee5e4c19ea388c5c96f0
saeed zeidan <saeed@pop-os.localdomain>
2023-01-10 23:21:29 +0200

    fix bugs + add utils.py

--- MainGame.py
+++ MainGame.py
@@ -1,3 +1,19 @@
 from Live import load_game, welcome
-print(welcome("Guy"))
-load_game()
+
+yes_choices = ['yes', 'y']
+no_choices = ['no', 'n']
+
+player_name = input("type your name:")
+
+welcome(f"{player_name}")
+
+while True:
+    load_game()
+    ans = input("do you want to play again (yes/no)?")
+    if ans.lower() in yes_choices:
+        continue
+    elif ans.lower() in no_choices:
+        break
+    else:
+        print('Type yes or no')
+        break
